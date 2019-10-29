660535d0235e23ed5f395d9b678928dabb6341bb
joseph townshend <joetownshend@mac.com>
2019-10-29 10:39:46 +0000

    feat: titles comonent set up and returning titles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Titles from './components/Titles';
 
 
 export default class App extends React.Component {
 
   render() {
     return (
-      <div>Hello World!</div>
+      <Titles />
     );
   }
 };
