ddda8461a1004fc6cc0dc428f4cac81b28eac405
joseph townshend <joetownshend@mac.com>
2019-10-29 10:43:22 +0000

    feat: form / weather components set up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components/Weather';
 
 
 export default class App extends React.Component {
 
   render() {
     return (
-      <Titles />
+      <div>
+        <Titles />
+        <Form />
+        <Weather />
+      </div>
     );
   }
 };
