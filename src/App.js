188b4f3b38569bad61ddc534a195be22100b4f5e
joseph townshend <joetownshend@mac.com>
2019-10-29 11:49:43 +0000

    feat: setState set up for all

--- src/App.js
+++ src/App.js
@@ -7,14 +7,33 @@ const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
 export default class App extends React.Component {
 
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+
   getWeather = async (event) => {
     event.preventDefault();
     const city = event.target.elements.city.value;
     const country = event.target.elements.country.value;
 
-    const API_CALL = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
-    const DATA = await API_CALL.json();
-    console.log(DATA);
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    const data = await api_call.json();
+
+    console.log(data);
+
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: ""
+    });
   }
 
   render() {
